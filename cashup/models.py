b23828f016859958bb1c2fc458ea9d42255e5264
Daniel Patrick <danieljudepatrick@gmail.com>
2016-12-14 07:29:26 +0000

    Create basic Vend Outlet and Register models for temporary storage of data

--- cashup/models.py
+++ cashup/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from social_django.models import UserSocialAuth
 
-# Create your models here.
+class Outlet(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+
+class Register(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    vend_user = models.ForeignKey(UserSocialAuth, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    outlet = models.ForeignKey(Outlet, on_delete=models.CASCADE)
+    is_open = models.BooleanField(default=False)
+    open_time = models.DateTimeField(blank=True, null=True)
+    close_time = models.DateTimeField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
