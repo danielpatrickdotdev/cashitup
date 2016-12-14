783bb0aaf19aec6fe8b3c7f84eea4e9ba6eb717b
Daniel Patrick <danieljudepatrick@gmail.com>
2016-12-14 18:56:10 +0000

    Add cashup app's models to django admin

--- cashup/admin.py
+++ cashup/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Outlet, Register, RegisterTakings
 
-# Register your models here.
+admin.site.register(Outlet)
+
+admin.site.register(Register)
+
+@admin.register(RegisterTakings)
+class RegisterTakingsAdmin(admin.ModelAdmin):
+    readonly_fields = ('register', 'register_open_time')
