35df561fe28b4ca493cbb2554a8313c138895b5c
Daniel Patrick <danieljudepatrick@gmail.com>
2016-12-15 10:21:47 +0000

    Add STATICFILES_DIRS under BASE_DIR (also change BASE_DIR back to default, creating SITE_DIR for next folder up)

--- cashitup/settings.py
+++ cashitup/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
-                os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'cashitup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database', 'cashitup.sqlite3'),
+        'NAME': os.path.join(SITE_DIR, 'database', 'cashitup.sqlite3'),
     }
 }
 
@@ -129,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(SITE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 # OAuth
 
